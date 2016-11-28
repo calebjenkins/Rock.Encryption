5f6e30c3b3a8ff89fe763a905f9b71143d3fa1a8
Brian Friesen <brianfriesen@quickenloans.com>
2016-11-28 12:01:27 -0500

    temp

---
+++ Rock.Encryption.IntegrationTests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using Rock.Encryption.Bcl;
+using Rock.Encryption.Bcl.Configuration.Xml;
+
+namespace Rock.Encryption.IntegrationTests
+{
+    public class Class1
+    {
+        [Test]
+        public void FooBar()
+        {
+            //var r = new RNGCryptoServiceProvider();
+
+            //var plainTextKey = new byte[32]; r.GetBytes(plainTextKey);
+            //var plainTextKeyString = Convert.ToBase64String(plainTextKey);
+            //// 
+
+            //var entropy = new byte[16]; r.GetBytes(entropy);
+            //var entropyString = Convert.ToBase64String(entropy);
+            //// 
+
+            //var cipherTextKey = ProtectedData.Protect(plainTextKey, entropy, DataProtectionScope.CurrentUser);
+            //var cipherTextKeyString = Convert.ToBase64String(cipherTextKey);
+            //// 
+
+            //var plainTextKey2 = ProtectedData.Unprotect(cipherTextKey, entropy, DataProtectionScope.CurrentUser);
+            //var plainTextKey2String = Convert.ToBase64String(plainTextKey2);
+            //// 
+
+            var encrypted = Crypto.Encrypt("Hello, world!", "unprotectedKey");
+            var decrypted = Crypto.Decrypt(encrypted, "protectedKey");
+
+            Assert.That(decrypted, Is.EqualTo("Hello, world!"));
+        }
+    }
+}
